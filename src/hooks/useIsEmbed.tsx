190913b9057ca3b05f51e771cebb661c8830ed56
nikhilrajaram <nikhilrajaram2@gmail.com>
2026-07-01 17:24:31 -0400

    whoops

--- src/hooks/useIsEmbed.tsx
+++ src/hooks/useIsEmbed.tsx
@@ -1,3 +1,6 @@
+/**
+ * Indicates whether or not the website should render as if embedded in another
+ */
 export function useIsEmbed() {
   return new URLSearchParams(window.location.search).get('embed') === 'true';
 }
